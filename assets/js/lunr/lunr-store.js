7e7bf0f4216b76a80d15b170e2d21f234e96ef14
daniek0617 <daniek0617@users.noreply.github.com>
2025-05-21 16:43:59 +0000

    deploy: 9eb42be526edfe66b1ff0eaef447655b43ccc320

--- assets/js/lunr/lunr-store.js
+++
@@ -1,7 +0,0 @@
-var store = [{
-        "title": "社会民族主义的基本特征",
-        "excerpt":"社会民族主义是一个描述当前中国社会现实性质的理论框架，具有复杂的经济、政治与文化特征。  ","categories": ["社会民族主义介绍"],
-        "tags": ["理论","意识形态"],
-        "url": "/sheminzuluntan.vip/%E7%A4%BE%E4%BC%9A%E6%B0%91%E6%97%8F%E4%B8%BB%E4%B9%89%E4%BB%8B%E7%BB%8D/2025/05/21/intro.html",
-        "teaser": null
-      }]
